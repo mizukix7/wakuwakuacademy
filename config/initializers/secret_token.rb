05976c990a764684c1ed7ff735989274f74350c6
Mizuki Seto <mizukix@nifty.com>
2013-11-28 07:19:46 +0900

    Rails + Bootstrap 初期ツリー

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wakuwakuacademy::Application.config.secret_key_base = '275bb86aab70b92818705e71aed060d9f3dae3708758e27f603818804c27e69ebba04fd89d111898eef89f9709b73915cad381f460234ff7db0852a033a7edec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wakuwakuacademy::Application.config.session_store :cookie_store, key: '_wakuwakuacademy_session'
