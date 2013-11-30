28ea0688464ef20c044f0ddc724a6240b48e8161
Mizuki Seto <mizukix@nifty.com>
2013-11-30 20:34:13 +0900

    タブをスペースに変換

--- app/controllers/itcommon_controller.rb
+++ app/controllers/itcommon_controller.rb
@@ -1,12 +1,12 @@
 class ItcommonController < ApplicationController
   # 大分類ごとのページを表示
   def largepage
-	  @movies = Movie.find_all_by_large_id(params[:id])
-	  large = LargeClass.find_by_large_id(params[:id])
-		if large
+    @movies = Movie.find_all_by_large_id(params[:id])
+    large = LargeClass.find_by_large_id(params[:id])
+    if large
       @large_name = params[:id] + " : " + large.name	
     else
       @large_name = "該当する分野はありません"
     end
-	end
+  end
 end
