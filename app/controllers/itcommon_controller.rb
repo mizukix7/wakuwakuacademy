af2dc3e8bf012231e59c014a608c86f9de92b778
Mizuki Seto <mizukix@nifty.com>
2013-12-08 18:04:35 +0900

    IT共通新着動画ページを作成 動画＆更新情報追加

--- app/controllers/itcommon_controller.rb
+++ app/controllers/itcommon_controller.rb
@@ -1,4 +1,8 @@
 class ItcommonController < ApplicationController
+  # 全体の新着順ページを表示
+	def index
+	  @movies = Movie.order("movie_id DESC")
+	end
   # 大分類ごとのページを表示
   def largepage
     @movies = Movie.find_all_by_large_id(params[:id])
